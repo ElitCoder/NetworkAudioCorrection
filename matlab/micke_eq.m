7c43dc810d25d78f28095ef7033c9a26222724ba
Niklas Carlsson <niklas.carlsson1994@gmail.com>
2018-04-25 11:21:44 +0200

    Update micke_eq.m

--- matlab/micke_eq.m
+++ matlab/micke_eq.m
@@ -40,33 +40,59 @@ max_total = max([max(powPx(2:length(powPx))), max(powPy(2:length(powPx)))]) + 3;
 x_mean = mean(powPx);
 y_mean = mean(powPy);
 
+set(0, 'DefaultAxesFontSize', 8);
 
-ax = subplot(3, 1, 1);
+ax = subplot(4, 1, 1);
 plot(ax,fx, powPx, 'r');
 axis(ax,[44, 22720, min_total, max_total]);
 set(ax, 'XScale', 'log')
 title(ax, 'Before');
-ylabel(ax, 'Magnitude (dB)');
-xlabel(ax, 'Frequency (Hz)');
+ylabel(ax, 'dB');
+xlabel(ax, 'Hz');
 grid on
 
-ay = subplot(3, 1, 2);
+ay = subplot(4, 1, 2);
 plot(ay, fy, powPy, 'g');
 axis(ay, [44, 22720, min_total, max_total]);
 set(ay, 'XScale', 'log')
 title(ay, 'After');
-ylabel(ay, 'Magnitude (dB)');
-xlabel(ay, 'Frequency (Hz)');
+ylabel(ay, 'dB');
+xlabel(ay, 'Hz');
 grid on
 
 %Adds a combined plot of both curves for comparing
-both = subplot(3, 1, 3);
+both = subplot(4, 1, 3);
 plot(both, fx, powPx, 'r');
 hold on
 plot(both, fy, powPy, 'g');
 axis(both, [44, 22720, min_total, max_total]);
 set(both, 'XScale', 'log');
 title(both, 'Combined');
-ylabel(both, 'Magnitude (dB)');
-xlabel(both, 'Frequency (Hz)');
+ylabel(both, 'dB');
+xlabel(both, 'Hz');
+grid on
+
+eq_file = fopen('eqs', 'r');
+formatSpec = '%f';
+A = fscanf(eq_file, formatSpec);
+num = A(1);
+A = A(2 : end);
+
+eq_matrix = zeros(num, 9);
+
+for i = 1:num
+    for j = 1:9
+        eq_matrix(i, j) = A((i - 1) * 9 + j);
+    end
+end
+
+eqs = subplot(4, 1, 4);
+plot(eqs, fx, powPx, 'r');
+hold on
+plot(eqs, fy, powPy, 'g');
+axis(eqs, [44, 22720, -15, 15]);
+set(eqs, 'XScale', 'log');
+title(eqs, 'EQs');
+ylabel(eqs, 'dB');
+xlabel(eqs, 'Hz');
 grid on
